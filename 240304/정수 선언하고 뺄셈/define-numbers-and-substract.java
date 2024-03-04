b46a40775362098d10cea480b9f11d4be948e248
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-04 23:12:13 +0900

    Title: 정수 선언하고 뺄셈, Time: 108ms, Memory: 8MB, Status: Passed - CodeTree

--- 240304/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 240304/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int a = 97, b = 13;
         int c = a-b;
-        System.out.print("a - b = c");
+        System.out.print(a + " - " + b + " = " + c);
     }
 }
