22fd73de37ca39c5da17f0e3b184d0e97b8418af
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-05 21:50:54 +0900

    Title: 시험 통과 여부 확인하기, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240305/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a > 80)
+        System.out.print("pass");
+
+        else {
+            int b = 80 - a;
+            System.out.printf("%d more score", b);
+        }
+    }
+}
