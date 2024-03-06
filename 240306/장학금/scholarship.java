a81e4699e2d0bf97e1da2c11d602bf024924a13e
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-06 12:58:19 +0900

    Title: 장학금, Time: 230ms, Memory: 10MB, Status: Passed - CodeTree

--- 240306/장학금/scholarship.java
+++ 240306/장학금/scholarship.java
@@ -8,9 +8,9 @@ public class Main {
         int b = sc.nextInt();
 
         if ( a >= 90 && b >= 95 )
-        System.out.print(10);
+        System.out.print(100000);
         else if ( a >= 90 && b >= 90 )
-        System.out.print(5);
+        System.out.print(50000);
         else
         System.out.print(0);
     }
