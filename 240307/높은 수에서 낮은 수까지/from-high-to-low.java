cca1b9e75c88cac261077031fb3a24e3c8ecefdc
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-07 23:36:23 +0900

    Title: 높은 수에서 낮은 수까지, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240307/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if ( a >= b ) {
+            while ( a >= b ) {
+                System.out.print( a + " " );
+                a--;
+            }
+        }
+        else {
+            while ( b >= a ) {
+                System.out.print( b + " " );
+                b--;
+            }
+        }
+    }
+}
