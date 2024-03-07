fe0e9740e00fc56dbd58f4845cbc88599447814c
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-07 22:26:49 +0900

    Title: 증가시키며 출력하기 3, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240307/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10, n = 26;
+
+        while ( i <= n ) {
+            System.out.print( i + " " );
+            i += 2;
+        }
+    }
+}
