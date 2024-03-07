a477270c694b95fd4524ec109c90c4403a6fdf93
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-07 14:33:43 +0900

    Title: 증가시키며 출력하기, Time: 109ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240307/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i;
+
+        for ( i = 5 ; i < 18 ; i += 2 )
+        System.out.print( i + " " );
+    }
+}
