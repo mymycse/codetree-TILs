ab2ad758ed563188616618cd1f31d65790b14d54
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-07 22:33:36 +0900

    Title: 감소시키며 출력하기 2, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240307/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 26, n = 10;
+
+        while ( i >= n ) {
+            System.out.print( i + " " );
+            i--;
+        }
+    }
+}
