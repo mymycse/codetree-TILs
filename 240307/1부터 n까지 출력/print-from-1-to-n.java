3345914de98864a0535b9319e82ebb527dc25788
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-07 14:31:22 +0900

    Title: 1부터 n까지 출력, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240307/1부터 n까지 출력/print-from-1-to-n.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n, i;
+        n = sc.nextInt();
+
+        for ( i = 1 ; i <= n ; i++ )
+        System.out.print(i + " ");
+    }
+}
