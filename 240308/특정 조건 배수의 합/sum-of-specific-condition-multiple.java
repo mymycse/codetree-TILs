094e7182d47cad585c8426683c697b15a5564eab
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-08 21:45:05 +0900

    Title: 특정 조건 배수의 합, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240308/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
+++ 240308/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -8,12 +8,18 @@ public class Main {
         int b = sc.nextInt();
         int sum = 0;
 
-        for ( int i = a ; i <= b ; i++ ) {
-            if ( i  % 5 == 0 ) {
+        if ( a < b ) {
+            for ( int i = a ; i <= b ; i++ ) {
+                if ( i  % 5 == 0 )
                 sum += i;
             }
         }
-
+        else {
+            for ( int i = b ; i <= a ; i++ ) {
+                if ( i % 5 == 0 )
+                sum += i;
+            }
+        }
         System.out.print(sum);
     }
 }
