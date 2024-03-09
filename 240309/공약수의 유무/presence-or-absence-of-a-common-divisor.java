f6dd9bc9e7a752d018f7d223be193e41cae54ac2
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-09 18:50:22 +0900

    Title: 공약수의 유무, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240309/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+
+        for ( int i = a ; i <= b ; i++ ) {
+            if ( 1920 % i == 0 && 2880 % i == 0 ) {
+                cnt++;
+                System.out.print(1);
+                break;
+            }
+        }
+        if ( cnt == 0 )
+        System.out.print(0);
+    }
+}
