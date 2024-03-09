f5f4d595eff51adc80e71a128d3755e0c66f5c94
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-09 18:40:43 +0900

    Title: 합성수 판별기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240309/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for ( int i = 2 ; i < n ; i++ ) {
+            if ( n % i == 0 ) {
+                cnt++;
+                System.out.print("C");
+                break;
+            }
+        }
+        if ( cnt == 0 )
+        System.out.print("NO");
+    }
+}
