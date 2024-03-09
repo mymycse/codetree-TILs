75b2224ce1d7fd180c2472989d030c530f1670af
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-09 16:04:45 +0900

    Title: 0 나올때까지 출력, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240309/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+
+        for (;;) {
+            n = sc.nextInt();
+            if ( n == 0 )
+            break;
+            System.out.println(n);
+        }
+    }
+}
