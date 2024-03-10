cfd3e73288fb3b9c341c6ecbc2f641572437201a
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-10 23:21:23 +0900

    Title: 별표 출력하기 11, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240310/별표 출력하기 11/print-star-11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for ( int i = 1 ; i <= n*2 + 1 ; i++ ) {
+            for ( int j = 1 ; j <= n*2 + 1 ; j++ ) {
+                if ( i % 2 == 1 )
+                System.out.print("* ");
+                else if ( j % 2 == 1 )
+                System.out.print("* ");
+                else
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
