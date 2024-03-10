ab109919efbfc2aa09aed5105f1ea43f5543dd14
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-10 22:23:41 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240310/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
+++ 240310/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -5,14 +5,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-
+        int cnt = 0;
+        
         for ( int i = 1 ; i <= n ; i++ ) {
-            for ( int j = n-1 ; j <= n-i+1 ; j++ )
+            for ( int j = 1 ; j <= n-i ; j++ ) {
                 System.out.print( "  " );
-            for ( int k = 1; k <= i ; k++ )
+                cnt++;
+            }
+            for ( int k = n - cnt ; k > 0 ; k-- ) {
                 System.out.print( "@ " );
+                cnt = 0;
+            }
             System.out.println();
         }
+    
         for ( int i = 1 ; i < n ; i++ ) {
             for ( int j = 1 ; j <= n-i ; j++ )
                 System.out.print( "@ " );
