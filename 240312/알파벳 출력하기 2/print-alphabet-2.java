bad2d18c3b3e0a8ccb0f8a3e1becf8fdf27e5466
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-12 10:05:46 +0900

    Title: 알파벳 출력하기 2, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240312/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 65;
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            for ( int j = 0 ; j < i ; j++ )
+            System.out.print("  ");
+            for ( int k = 0 ; k < n - i ; k++ ) {
+                System.out.print((char)cnt+" ");
+                cnt++;
+                if ( cnt > 90 )
+                cnt = 65;
+            }
+            System.out.println();
+        }
+    }
+}
