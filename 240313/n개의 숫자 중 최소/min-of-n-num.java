35fd641f60426d73081a107b05c740f633202318
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-13 23:16:23 +0900

    Title: n개의 숫자 중 최소, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240313/n개의 숫자 중 최소/min-of-n-num.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+        int[] arr = new int[n];
+        int minVal = Integer.MAX_VALUE;
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            arr[i] = sc.nextInt();
+            if ( arr[i] < minVal ) 
+                minVal = arr[i];
+        }
+        for ( int i = 0 ; i < n ; i++ ) {
+            if ( arr[i] == minVal )
+            cnt++;
+        }
+        System.out.printf("%d %d", minVal, cnt);
+    }
+}
