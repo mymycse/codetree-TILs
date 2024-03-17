14705f306bf9fc808560ffdfc79f0e0127d57156
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-17 21:22:37 +0900

    Title: 공백을 기준으로 출력, Time: 277ms, Memory: 10MB, Status: Passed - CodeTree

--- 240317/공백을 기준으로 출력/output-based-on-space.java
+++ 240317/공백을 기준으로 출력/output-based-on-space.java
@@ -4,9 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNextLine()) {
-            String s = sc.next();
-            System.out.print(s);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        for ( int i = 0 ; i < s1.length() ; i++ ) {
+            char c = s1.charAt(i);
+            if ( c != ' ' )
+            System.out.print(c);
+        }
+
+        for ( int i = 0 ; i < s2.length() ; i++ ) {
+            char c = s2.charAt(i);
+            if ( c != ' ' )
+            System.out.print(c);
         }
     }
 }
