afdfcda909421d3c5d0936017a4d5db5ec2ae5af
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-17 19:17:09 +0900

    Title: 격자 반대로 채우기, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240317/격자 반대로 채우기/grid-anti-fill.java
+++ 240317/격자 반대로 채우기/grid-anti-fill.java
@@ -10,17 +10,35 @@ public class Main {
 
         int cnt = n*n;
 
-        for ( int j = 0 ; j < n ; j++ ) {
-            if ( j % 2 == 0 ) {
-                for ( int i = n-1 ; i >= 0 ; i-- ) {
-                    arr[i][j] = cnt;
-                    cnt--;
+        if ( n % 2 == 0 ) {
+            for ( int j = 0 ; j < n ; j++ ) {
+                if ( j % 2 == 0 ) {
+                    for ( int i = n-1 ; i >= 0 ; i-- ) {
+                        arr[i][j] = cnt;
+                        cnt--;
+                    }
+                }
+                else {
+                    for ( int i = 0 ; i < n ; i++ ) {
+                        arr[i][j] = cnt;
+                        cnt--;
+                    }
                 }
             }
-            else {
-                for ( int i = 0 ; i < n ; i++ ) {
-                    arr[i][j] = cnt;
-                    cnt--;
+        }
+        else {
+            for ( int j = 0 ; j < n ; j++ ) {
+                if ( j % 2 != 0 ) {
+                    for ( int i = n-1 ; i >= 0 ; i-- ) {
+                        arr[i][j] = cnt;
+                        cnt--;
+                    }
+                }
+                else {
+                    for ( int i = 0 ; i < n ; i++ ) {
+                        arr[i][j] = cnt;
+                        cnt--;
+                    }
                 }
             }
         }
