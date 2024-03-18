603326696d4491ae3e4a9e653aff26e6746bd71a
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-18 17:42:08 +0900

    Title: a로 채워넣기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240318/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+
+        arr[1] = 'a';
+        arr[str.length()-2] = 'a';
+
+        str = String.valueOf(arr);
+
+        System.out.print(str);
+    }
+}
