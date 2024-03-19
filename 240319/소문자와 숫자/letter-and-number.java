248065d4488e0b65574afc3d9b1fe1134faf7c43
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-19 12:37:28 +0900

    Title: 소문자와 숫자, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240319/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int s = str.length();
+
+        for ( int i = 0 ; i < s ; i++ ) {
+            char x = str.charAt(i);
+
+            if ( 'A' <= x && x <= 'Z' ) {
+                int k = (char)x;
+                System.out.print((char)(k+32));
+            }
+            else if ( 'a' <= x && x <= 'z' || '0' <= x && x <= '9' )
+                System.out.print(x);
+        }
+    }
+}
