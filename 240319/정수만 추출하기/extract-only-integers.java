54d69797e6ca6b4b5cb222f2ad0155d6352b9467
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-19 12:48:44 +0900

    Title: 정수만 추출하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240319/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        String a = "";
+        String b = "";
+
+        for ( int i = 0 ; i < str1.length() ; i++ ) {
+            char x = str1.charAt(i);
+            if ( '0' <= x && x <= '9' )
+                a += str1.charAt(i);
+            else
+                break;
+        }
+
+        for ( int i = 0 ; i < str2.length() ; i++ ) {
+            char x = str2.charAt(i);
+            if ( '0' <= x && x <= '9' )
+                b += str2.charAt(i);
+            else
+                break;
+        }
+
+        int A = Integer.parseInt(a);
+        int B = Integer.parseInt(b);
+
+        System.out.print(A+B);
+    }
+}
