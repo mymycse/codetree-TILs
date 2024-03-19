cbca12a75311924a0a802897ec7740e48cd94f81
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-19 12:26:52 +0900

    Title: 그 다음 알파벳, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240319/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+
+        if ( c == 'z' )
+            System.out.print('a');
+        else {
+            int a = (int)c;
+            System.out.print((char)(a+1));
+        }
+    }
+}
