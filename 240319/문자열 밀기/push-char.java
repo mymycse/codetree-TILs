9f8f3b18fe8b80a8de5176892e828a3abc195f7f
mymycse <162075734+mymycse@users.noreply.github.com>
2024-03-19 10:44:08 +0900

    Title: 문자열 밀기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240319/문자열 밀기/push-char.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int n = s.length();
+
+        System.out.print( s.substring(1) + s.substring(0,1));
+    }
+}
