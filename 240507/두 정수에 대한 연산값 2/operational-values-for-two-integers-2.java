c8d2245634c318b871ce65eb8d46709452583ff6
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-07 15:58:13 +0900

    Title: 두 정수에 대한 연산값 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240507/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
+++ 240507/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int max(int a) {
+        return a*2;
+    }
+    public static int min(int b) {
+        return b+10;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
         if ( a > b )
-            System.out.print(a*2 + " " + b+10);
+            System.out.print(max(a) + " " + min(b));
         else
-            System.out.print(a+10 + " " + b*2);
+            System.out.print(min(a) + " " + max(b));
     }
 }
