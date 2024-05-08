74f5c4baa2e1cf8eff9a1509b623366ed4497b61
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-08 11:44:17 +0900

    Title: 문자열 정렬, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240508/문자열 정렬/string-sort.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        String sortedStr = new String(chars);
+        System.out.print(sortedStr);
+    }
+}
