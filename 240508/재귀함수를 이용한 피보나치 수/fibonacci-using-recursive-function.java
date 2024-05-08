beb9977fde40ded60075a4d4993bb16a7ba98a9a
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-08 13:49:59 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240508/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int fib ( int n ) {
+        if ( n == 1 || n == 2 )
+            return 1;
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.print(fib(n));
+    }
+}
