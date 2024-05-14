954b8412647fffeaaef16981ab578c3d81efba3a
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-14 17:36:12 +0900

    Title: Time to Time, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240514/Time to Time/time-to-time.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int time = ;
+
+        int hf = sc.nextInt();
+        int mf = sc.nextInt();
+        int hl = sc.nextInt();
+        int ml = sc.nextInt();
+
+        while ( true ) {
+            if ( hf == hl && mf == ml )
+                break;
+            mf++;
+            time++;
+            if ( mf == 60 ) {
+                hf++;
+                mf = 0;
+            }
+        }
+
+        System.out.print(time);
+    }
+}
