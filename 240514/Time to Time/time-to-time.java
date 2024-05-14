98a417bc86462fbf3ab4c1bb6376e8923424e674
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-14 17:36:39 +0900

    Title: Time to Time, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240514/Time to Time/time-to-time.java
+++ 240514/Time to Time/time-to-time.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int time = ;
+        int time = 0;
 
         int hf = sc.nextInt();
         int mf = sc.nextInt();
