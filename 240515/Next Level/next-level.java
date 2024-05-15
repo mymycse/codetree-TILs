8e905005dc42d93e2b9e59efa9f40d6873170697
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-15 23:28:21 +0900

    Title: Next Level, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240515/Next Level/next-level.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Student st1 = new Student();
+        System.out.printf("user %s lv %d\n", st1.id, st1.lv);
+
+        Student st2 = new Student("hello", 28);
+        System.out.printf("user %s lv %d", st2.id, st2.lv);
+    }
+}
+class Student {
+    String id;
+    int lv;
+    
+    public Student() {
+        this.id = "codetree";
+        this.lv = 10;
+    }
+    public Student ( String str, int n ) {
+        this.id = str;
+        this.lv = n;
+    }
+}
