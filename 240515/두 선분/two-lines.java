ac0ef7e036ec6e4e994c82f3473b00cd8053c81a
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-15 23:18:44 +0900

    Title: 두 선분, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240515/두 선분/two-lines.java
+++ 240515/두 선분/two-lines.java
@@ -9,7 +9,7 @@ public class Main {
         int x3 = sc.nextInt();
         int x4 = sc.nextInt();
 
-        if ( x3 <= x2 || x1 >= x4 )
+        if ( x3 >= x1 && x3 <= x2 || x1 <= x4 && x4 <= x2 )
             System.out.print("intersecting");
         else
             System.out.print("nonintersecting");
