1f1c44ee8736eea9d8e97fef73e533decccd5b1b
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-18 12:25:21 +0900

    Title: 요일 맞추기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240518/요일 맞추기/guess-day-of-week.java
+++ 240518/요일 맞추기/guess-day-of-week.java
@@ -33,7 +33,6 @@ public class Main {
 
             while ( true ) {
                 if ( m1 == m2 && d1 == d2 ) {
-                    elapsedDays++;
                     break;
                 }
 
