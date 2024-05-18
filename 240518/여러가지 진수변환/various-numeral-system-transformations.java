b6c0cb85d36865690ec14d81c16e18956590594e
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-18 20:30:17 +0900

    Title: 여러가지 진수변환, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240518/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] digits = new int[20];
+        int cnt = 0;
+
+        while ( true ) {
+            if ( n < k ) {
+                digits[cnt++] = n;
+                break;
+            }
+
+            digits[cnt++] = n%k;
+            n /= k;
+        }
+
+        for ( int i = cnt-1 ; i >= 0 ; i-- )
+            System.out.print(digits[i]);
+    }
+}
