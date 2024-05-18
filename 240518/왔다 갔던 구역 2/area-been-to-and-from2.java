a63ae2f53ba9c2450c099517fa78d770725e5230
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-18 22:28:33 +0900

    Title: 왔다 갔던 구역 2, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240518/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 1000;
+        int cnt = 0;
+        int[] map = new int[2001];
+
+        while( n-- > 0 ) {
+            int a = sc.nextInt();
+            char b = sc.next().charAt(0);
+
+            while ( a-- > 0 ){
+                if ( b == 'R' )
+                    map[x++]++;
+                else
+                    map[x--]++;
+            }
+        }
+
+        Arrays.sort(map);
+
+        for ( int i = 2000 ; i >= 0 ; i-- ){
+            if ( map[i] <= 1 )
+                break;
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
