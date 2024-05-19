b3cc26bc5998d6bdf30ce78b7c83d8fb24fefcea
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-19 23:49:40 +0900

    Title: 왔다 갔던 구역 2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240519/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240519/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,31 +1,30 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int n = sc.nextInt();
         int x = 1000;
         int cnt = 0;
         int[] map = new int[2001];
 
-        while( n-- > 0 ) {
+        while (n-- > 0) {
             int a = sc.nextInt();
             char b = sc.next().charAt(0);
 
-            for ( int i = 0 ; i < a ; i++ ) {
-                if ( b == 'R' )
+            for (int i = 0; i < a; i++) {
+                if (b == 'R')
                     map[++x]++;
                 else
                     map[--x]++;
             }
         }
-
-        for ( int i = 0 ; i < map.length ; i++ ){
-            if ( map[i] >= 2 )
+        
+        for (int i = 0; i < map.length; i++) {
+            if (map[i] >= 2)
                 cnt++;
-            
-        }
+        }  
 
         System.out.println(cnt);
     }
