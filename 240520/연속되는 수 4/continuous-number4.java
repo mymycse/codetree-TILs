a1833c233ecec2ffabd8b21a690bc09e637524ea
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-20 12:51:52 +0900

    Title: 연속되는 수 4, Time: 201ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240520/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        int ans = 0;
+        int[] arr = new int[n];
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            arr[i] = sc.nextInt();
+
+            if ( i > 0 && arr[i] > arr[i-1] )   
+                cnt++;
+            else
+                cnt = 1;
+
+            ans = Math.max(ans,cnt);
+        }
+
+        System.out.println(ans);
+    }
+}
