d5f7ffb4ebe92e2ed77507144484b897f8e4e248
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-20 10:55:15 +0900

    Title: 신기한 타일 뒤집기, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240520/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] flip = new int[200_001];
+        int loc = 100_000;
+
+        while ( n-- > 0 ) {
+            int x = sc.nextInt();
+            char c = sc.next().charAt(0);
+
+            switch(c) {
+                case 'R' :
+                    while ( x-- > 0 ) {
+                        flip[loc++] = 1;
+                        if ( x == 1 )
+                            loc--;
+                    }
+                    break;
+                case 'L' :
+                    while ( x-- > 0 ) {
+                        flip[loc--] = -1;
+                        if ( x == 1 )
+                            loc++;
+                    }
+                    break;
+            }
+        }
+
+        int white = 0;
+        int black = 0;
+
+        for ( int i : flip ) {
+            if ( i == 1 )
+                black++;
+            else if ( i == -1 )
+                white++;
+        }
+
+        System.out.print(black + " " + white);
+    }
+}
