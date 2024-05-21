c1e2e697f62e393e992d57bf08b4f9de5961f851
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-21 11:07:41 +0900

    Title: Carry 피하기 2, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240521/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = -1;
+        int[] num = new int[n];
+        for ( int i = 0 ; i < n ; i++ )
+            num[i] = sc.nextInt();
+
+        for ( int i = 0 ; i < n ; i++ )
+            for ( int j = i+1 ; j < n ; j++ )
+                for ( int k = j+1 ; k < n ; k++ ) {
+                    int a = num[i]; int b = num[j]; int c = num[k];
+                    boolean carry = false;
+
+                    while ( true ) {
+                        if ( a%10 + b%10 + c%10 > 9 ) {
+                            carry = true; 
+                            break;
+                        }
+                        a /= 10; 
+                        b /= 10; 
+                        c /= 10;
+                        
+                        if ( a == 0 && b == 0 && c == 0 )
+                            break;
+                    }
+
+                    if ( !carry )
+                        ans = Math.max(ans, num[i]+num[j]+num[k]);
+                }
+        System.out.println(ans);
+    }
+}
