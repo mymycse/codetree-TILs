cd4795e0120aea02d8b9132f03bbc1ae3c54d488
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-21 12:26:51 +0900

    Title: 오목, Time: 187ms, Memory: 10MB, Status: Passed - CodeTree

--- 240521/오목/O-mok.java
+++ 240521/오목/O-mok.java
@@ -5,56 +5,66 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int[][] map = new int[19][19];
 
-        for ( int i = 0 ; i < 19 ; i++ )
-            for ( int j = 0 ; j < 19 ; j++ )
+        // 바둑판 입력
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 19; j++) {
                 map[i][j] = sc.nextInt();
+            }
+        }
 
         int win = 0;
         int r = 0;
         int c = 0;
 
-        // 가로
-        for ( int i = 0 ; i < 19 ; i++ )
-            for ( int j = 0 ; j < 16 ; j++ )
-                if ( map[i][j] != 0 && map[i][j] == map[i][j+1] && map[i][j+1] == map[i][j+2] &&
-                map[i][j+2] == map[i][j+3] && map[i][j+3] == map[i][j+4] ) {
-                    win = map[i][j];
-                    r = i+1; c = j+3;
-                }
-        
-        // 세로
-        if ( win == 0 )
-        for ( int i = 0 ; i < 16 ; i++ )
-            for ( int j = 0 ; j < 19 ; j++ )
-                if ( map[i][j] != 0 && map[i][j] == map[i+1][j] && map[i+1][j] == map[i+2][j] &&
-                map[i+2][j] == map[i+3][j] && map[i+3][j] == map[i+4][j] ) {
-                    win = map[i][j];
-                    r = i+3; c = j+1;
-                }
+        // 방향 배열: 가로, 세로, 오른쪽 아래 대각선, 왼쪽 아래 대각선
+        int[][] directions = {{0, 1}, {1, 0}, {1, 1}, {1, -1}};
 
-        // 대각선1
-        if ( win == 0 )
-        for ( int i = 0 ; i < 16 ; i++ )
-            for ( int j = 0 ; j < 16 ; j++ )
-                if ( map[i][j] != 0 && map[i][j] == map[i+1][j+1] && map[i+1][j+1] == map[i+2][j+2] &&
-                map[i+2][j+2] == map[i+3][j+3] && map[i+3][j+3] == map[i+4][j+4] ) {
-                    win = map[i][j];
-                    r = i+3; c = j+3;
-                }
+        outerLoop:
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 19; j++) {
+                if (map[i][j] != 0) {
+                    int color = map[i][j];
 
-        // 대각선2
-        if ( win == 0 )
-        for ( int i = 0 ; i < 16 ; i++ )
-            for ( int j = 4 ; j < 19 ; j++ )
-                if ( map[i][j] != 0 && map[i][j] == map[i+1][j-1] && map[i+1][j-1] == map[i+2][j-2] &&
-                map[i+2][j-2] == map[i+3][j-3] && map[i+3][j-3] == map[i+4][j-4] ) {
-                    win = map[i][j];
-                    r = i+3 ; c = j-1;
+                    for (int[] direction : directions) {
+                        int count = 1;
+                        int x = i + direction[0];
+                        int y = j + direction[1];
+
+                        while (count < 5 && x >= 0 && x < 19 && y >= 0 && y < 19 && map[x][y] == color) {
+                            count++;
+                            x += direction[0];
+                            y += direction[1];
+                        }
+
+                        if (count == 5) {
+                            // 이전 위치 체크
+                            int prevX = i - direction[0];
+                            int prevY = j - direction[1];
+                            if (prevX >= 0 && prevX < 19 && prevY >= 0 && prevY < 19 && map[prevX][prevY] == color) {
+                                continue;
+                            }
+
+                            // 다음 위치 체크
+                            int nextX = x;
+                            int nextY = y;
+                            if (nextX >= 0 && nextX < 19 && nextY >= 0 && nextY < 19 && map[nextX][nextY] == color) {
+                                continue;
+                            }
+
+                            win = color;
+                            r = i + direction[0] * 2 + 1; // 중앙 위치
+                            c = j + direction[1] * 2 + 1;
+                            break outerLoop;
+                        }
+                    }
                 }
+            }
+        }
 
         System.out.println(win);
-        
-        if ( win != 0 )
+
+        if (win != 0) {
             System.out.println(r + " " + c);
+        }
     }
 }
