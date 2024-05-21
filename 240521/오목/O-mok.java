0f93ce7ed0f928f20a0537094d9fc32e1ede653f
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-21 12:18:17 +0900

    Title: 오목, Time: 155ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240521/오목/O-mok.java
+++ 240521/오목/O-mok.java
@@ -32,7 +32,7 @@ public class Main {
                     r = i+3; c = j+1;
                 }
 
-        // 대각선
+        // 대각선1
         if ( win == 0 )
         for ( int i = 0 ; i < 16 ; i++ )
             for ( int j = 0 ; j < 16 ; j++ )
@@ -40,6 +40,16 @@ public class Main {
                 map[i+2][j+2] == map[i+3][j+3] && map[i+3][j+3] == map[i+4][j+4] ) {
                     win = map[i][j];
                     r = i+3; c = j+3;
+                }
+
+        // 대각선2
+        if ( win == 0 )
+        for ( int i = 0 ; i < 16 ; i++ )
+            for ( int j = 4 ; j < 19 ; j++ )
+                if ( map[i][j] != 0 && map[i][j] == map[i+1][j-1] && map[i+1][j-1] == map[i+2][j-2] &&
+                map[i+2][j-2] == map[i+3][j-3] && map[i+3][j-3] == map[i+4][j-4] ) {
+                    win = map[i][j];
+                    r = i+3 ; c = j-2;
                 }
 
         System.out.println(win);
