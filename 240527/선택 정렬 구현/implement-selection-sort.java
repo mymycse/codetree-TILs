97efe0e34667b66e98455b631932868f97025036
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-27 16:20:42 +0900

    Title: 선택 정렬 구현, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240527/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for ( int i = 0 ; i < n ; i++ )
+            arr[i] = sc.nextInt();
+
+        for ( int i = 0 ; i < n-1 ; i++ ) {
+            int min = i;
+            for ( int j = i ; j < n ; j++ ) 
+                if ( arr[j] < arr[min] )
+                    min = j;
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        for( int t : arr )
+            System.out.print( t + " " ); 
+    }
+}
