feb7f799ab19e182607e85fbce9cf63354fc759d
my <162075734+mymycse@users.noreply.github.com>
2024-06-10 22:30:01 +0900

    Title: 재귀함수의 꽃, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240610/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void num(int n) {
+        if ( n == 0 )
+            return;
+        System.out.print( n + " " );
+        num(n-1);
+        System.out.print( n + " " );
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        num(n);
+    }
+}
