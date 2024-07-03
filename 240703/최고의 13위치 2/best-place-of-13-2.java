17800305ba5b52682171ae56baa68ec5a9155dab
my <162075734+mymycse@users.noreply.github.com>
2024-07-03 14:30:44 +0900

    Title: 최고의 13위치 2, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240703/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] coin = new int[n][n];
+
+        for ( int i = 0 ; i < n ; i++ )
+            for ( int j = 0 ; j < n ; j++ )
+                coin[i][j] = sc.nextInt();
+
+        int[][] map = new int[n][n-2];
+
+        for ( int i = 0 ; i < n ; i++ )
+            for ( int j = 0 ; j < n-2 ; j++ )
+                map[i][j] = coin[i][j]+coin[i][j+1]+coin[i][j+2];
+
+        int result = 0;
+
+        for ( int i = 0 ; i < n ; i++ )
+            for ( int j = 0 ; j < n-2 ; j++ )
+                for ( int r = i ; r < n ; r++ )
+                    for ( int c = 0 ; c < n-2 ; c++ ) {
+                        if ( i == r && Math.abs(j-c) < 2 )
+                            continue;
+                        result = Math.max(result,map[i][j]+map[r][c]);
+                    }
+
+        System.out.println(result);
+    }
+}
