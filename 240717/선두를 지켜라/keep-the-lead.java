0c12fcd806ef095b3b6c97388a6c3eb3b1a5f060
my <162075734+mymycse@users.noreply.github.com>
2024-07-17 14:50:44 +0900

    Title: 선두를 지켜라, Time: 202ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240717/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] temp = new int[n][2];
+        int totalTime = 0;
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            temp[i][0] = sc.nextInt();
+            temp[i][1] = sc.nextInt();
+
+            totalTime += temp[i][1];
+        }
+
+        int[] posA = new int[totalTime+1];
+        int[] posB = new int[totalTime+1];
+
+        int currentTime = 0;
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            int v = temp[i][0];
+            int t = temp[i][1];
+
+            for ( int j = 0 ; j < t ; j++ ) {
+                currentTime++;
+                posA[currentTime] = posA[currentTime-1] + v;
+            }
+        }
+
+        currentTime = 0;
+
+        for ( int i = 0 ; i < m ; i++ ) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for ( int j = 0 ; j < t ; j++ ) {
+                currentTime++;
+                posB[currentTime] = posB[currentTime-1] + v;
+            }
+        }
+
+        int leadChanges = 0;
+        int currentLeader = 0;
+
+        for ( int i = 0 ; i < totalTime ; i++ ) {
+            if (posA[i] > posB[i]) {
+                if (currentLeader != 1) {
+                    leadChanges++;
+                    currentLeader = 1;
+                }
+            } else if (posB[i] > posA[i]) {
+                if (currentLeader != 2) {
+                    leadChanges++;
+                    currentLeader = 2;
+                }
+            }
+        }
+
+        System.out.println(leadChanges-1);
+    }
+}
