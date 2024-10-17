a5f19de6b98a6a80fac0b9756233465096ffb11c
my <162075734+mymycse@users.noreply.github.com>
2024-10-17 16:02:06 +0900

    Title: 제곱근 사이의 수를 구하는 함수, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241017/제곱근 사이의 수를 구하는 함수/function-to-find-number-between-square-roots.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double d1 = Math.sqrt(Double.parseDouble(st.nextToken()));
+        double d2 = Math.sqrt(Double.parseDouble(st.nextToken()));
+
+        int cnt = (int)d1 - (int)d2;
+
+        System.out.println(cnt);
+    }
+}
